ad317add0426d8976378f038714be712f1870ce8
nguyenworkingg-sketch <nguyenworkingg@gmail.com>
2026-09-16 14:57:52 +0700

    Configure default Google Sheets live endpoint

---
+++ live-config.js
@@ -0,0 +1,6 @@
+// Default live bridge for Investment Data Hub.
+// This Web App reads the POW / FPT / REE tabs from the user's Google Sheet.
+localStorage.setItem(
+  'investment_data_hub_apps_script_url_v1',
+  'https://script.google.com/macros/s/AKfycbz7wdDa_KO0Lqh0C5H2kGpBPImy05n4U03TOdZfDE5wlCrEUjYoG2_pSMHvw8rHfM8jhg/exec'
+);
